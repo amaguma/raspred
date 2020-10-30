6fccf54f43df21aa8282f05c3ae3846c7079f332
amaguma <amaguma5151@gmail.com>
2020-10-31 02:28:38 +0300

    Scripted auto-commit on change (2020-10-31 02:28:38) by gitwatch.sh

--- lab2/src/main/java/AirportReducer.java
+++ lab2/src/main/java/AirportReducer.java
@@ -2,7 +2,9 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
+import java.io.IOException;
+
 public class AirportReducer extends Reducer<AirportWritable, Text, IntWritable, Text> {
     @Override
-    protected void reduce(AirportWritable key, Iterable<Text> values)
+    protected void reduce(AirportWritable key, Iterable<Text> values, Context context) throws IOException, 
 }
