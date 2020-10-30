d34ef7b4cf8701b9552d9958fe9c1062c1957761
amaguma <amaguma5151@gmail.com>
2020-10-31 02:42:20 +0300

    Scripted auto-commit on change (2020-10-31 02:42:19) by gitwatch.sh

--- lab2/src/main/java/AirportReducer.java
+++ lab2/src/main/java/AirportReducer.java
@@ -26,6 +26,6 @@ public class AirportReducer extends Reducer<AirportWritable, Text, IntWritable, 
             }
         }
         sum /= count;
-        context.write();
+        context.write(new Text(airportName + ));
     }
 }
