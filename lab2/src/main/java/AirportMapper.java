d0f227f69ed1ccb864294e3946fa665845136cd5
amaguma <amaguma5151@gmail.com>
2020-10-31 01:43:15 +0300

    Scripted auto-commit on change (2020-10-31 01:43:15) by gitwatch.sh

--- lab2/src/main/java/AirportMapper.java
+++ lab2/src/main/java/AirportMapper.java
@@ -6,16 +6,13 @@ import java.io.IOException;
 
 public class AirportMapper extends Mapper<LongWritable, Text, AirportWritable, Text> {
 
-    AirportWritable awKey = new AirportWritable();
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String[] columns = value.toString().split(",");
         if (key.get() > 0) {
             int id = Integer.parseInt(columns[0].replaceAll("\"",""));
-            awKey.setId(id);
-            awKey.setIndicator(0);
             String name = columns[1].replaceAll("\"","");
-            context.write(awKey, new Text(name));
+            context.write(new AirportWritable(id, 0), new Text(name));
         }
     }
 }
--- lab2/src/main/java/FlightMapper.java
+++ lab2/src/main/java/FlightMapper.java
@@ -5,20 +5,17 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 public class FlightMapper extends Mapper<LongWritable, Text, AirportMapper, Text> {
-    private static final int ID = 14;
-    private static final int DELAY_TIME = 18;
 
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String[] columns = value.toString().split(",");
         if (key.get() > 0) {
-            int id = Integer.parseInt(columns[ID]);
-            float delay = Float.parseFloat(columns[DELAY_TIME]);
+            int id = Integer.parseInt(columns[14]);
+            float delay = Float.parseFloat(columns[18]);
             if (delay == 0) {
                 return;
             }
-
-            
+            context.write(new AirportMapper(id, ));
         }
     }
 }
