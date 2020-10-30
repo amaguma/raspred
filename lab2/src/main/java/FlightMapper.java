46d10571b6ca7d141d7561e5fc7887479c7c7bed
amaguma <amaguma5151@gmail.com>
2020-10-31 01:30:01 +0300

    Scripted auto-commit on change (2020-10-31 01:30:01) by gitwatch.sh

--- lab2/src/main/java/FlightMapper.java
+++ lab2/src/main/java/FlightMapper.java
@@ -1,5 +1,7 @@
 import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class FlightMapper extends Mapper<LongWritable, Text, AirportMapper, > {
-}
+public class FlightMapper extends Mapper<LongWritable, Text, AirportMapper, Text> {
+
+} 
