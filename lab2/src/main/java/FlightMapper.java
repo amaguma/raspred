ce31087cd35c45d33051c8e042d6128b9222d366
amaguma <amaguma5151@gmail.com>
2020-10-31 01:31:07 +0300

    Scripted auto-commit on change (2020-10-31 01:31:07) by gitwatch.sh

--- lab2/src/main/java/FlightMapper.java
+++ lab2/src/main/java/FlightMapper.java
@@ -3,5 +3,6 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 public class FlightMapper extends Mapper<LongWritable, Text, AirportMapper, Text> {
-
-} 
+    private static final int ID = 14;
+    private static final int AIRPORT_DELAY = 18;
+}
