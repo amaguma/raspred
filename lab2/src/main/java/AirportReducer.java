63ac42702236c9c235f23d9adbfcb5930c194586
amaguma <amaguma5151@gmail.com>
2020-10-31 02:26:42 +0300

    Scripted auto-commit on change (2020-10-31 02:26:42) by gitwatch.sh

--- lab2/src/main/java/AirportReducer.java
+++ lab2/src/main/java/AirportReducer.java
@@ -3,5 +3,6 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 public class AirportReducer extends Reducer<AirportWritable, Text, IntWritable, Text> {
+    @Override
     
 }
