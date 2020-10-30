515ce00456cc5749113c44bb629358a03a5408d2
amaguma <amaguma5151@gmail.com>
2020-10-31 01:33:41 +0300

    Scripted auto-commit on change (2020-10-31 01:33:41) by gitwatch.sh

--- lab2/src/main/java/FlightMapper.java
+++ lab2/src/main/java/FlightMapper.java
@@ -2,7 +2,17 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
+import java.io.IOException;
+
 public class FlightMapper extends Mapper<LongWritable, Text, AirportMapper, Text> {
     private static final int ID = 14;
-    private static final int AIRPORT_DELAY = 18;
+    private static final int DELAY_TIME = 18;
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String[] columns = value.toString().split(",");
+        if (key.get() > 0) {
+            
+        }
+    }
 }
