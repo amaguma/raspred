5130af8e606e914790df791cf7e598b6a584f672
amaguma <amaguma5151@gmail.com>
2020-10-31 00:47:07 +0300

    Scripted auto-commit on change (2020-10-31 00:47:06) by gitwatch.sh

--- lab2/src/main/java/AirportMapper.java
+++ lab2/src/main/java/AirportMapper.java
@@ -2,7 +2,9 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
+import java.io.IOException;
+
 public class AirportMapper extends Mapper<LongWritable, Text, AirportWritable, Text> {
     @Override
-    protected void map(LongWritable key, Text text, Context context) throws 
+    protected void map(LongWritable key, Text text, Context context) throws IOException
 }
