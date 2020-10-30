9bb60cee7fa9ef12e517d4ff8977d3c54ce80666
amaguma <amaguma5151@gmail.com>
2020-10-31 02:31:37 +0300

    Scripted auto-commit on change (2020-10-31 02:31:36) by gitwatch.sh

--- lab2/src/main/java/AirportReducer.java
+++ lab2/src/main/java/AirportReducer.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 public class AirportReducer extends Reducer<AirportWritable, Text, IntWritable, Text> {
     @Override
     protected void reduce(AirportWritable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-        String airportName = 
+        
+        String airportName =
     }
 }
