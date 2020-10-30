041112d10c692c3c73bd121cba76dd259a3c60ea
amaguma <amaguma5151@gmail.com>
2020-10-31 00:12:19 +0300

    Scripted auto-commit on change (2020-10-31 00:12:18) by gitwatch.sh

--- lab2/src/main/java/AirportWritable.java
+++ lab2/src/main/java/AirportWritable.java
@@ -4,5 +4,5 @@ import org.apache.hadoop.io.WritableComparable;
 public class AirportWritable implements Writable, WritableComparable<AirportWritable> {
 
     private int id;
-    private 
+    private int 
 }
