0b1c8d7c39ebbf1f31cdc49e13fb27708b99f555
amaguma <amaguma5151@gmail.com>
2020-10-31 02:37:12 +0300

    Scripted auto-commit on change (2020-10-31 02:37:12) by gitwatch.sh

--- lab2/src/main/java/AirportReducer.java
+++ lab2/src/main/java/AirportReducer.java
@@ -15,7 +15,7 @@ public class AirportReducer extends Reducer<AirportWritable, Text, IntWritable, 
         double count = 0;
         double sum = 0;
         while(iterator.hasNext()) {
-            double delay = Double.parseDouble(iterator.hasNext())
+            double delay = Double.parseDouble(iterator.next())
         }
     }
 }
