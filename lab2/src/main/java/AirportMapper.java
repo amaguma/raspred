9ab8787b474784f5d14cf8be0b532f73b5768580
amaguma <amaguma5151@gmail.com>
2020-10-31 01:16:10 +0300

    Scripted auto-commit on change (2020-10-31 01:16:09) by gitwatch.sh

--- lab2/src/main/java/AirportMapper.java
+++ lab2/src/main/java/AirportMapper.java
@@ -6,12 +6,12 @@ import java.io.IOException;
 
 public class AirportMapper extends Mapper<LongWritable, Text, AirportWritable, Text> {
 
-    
+    AirportWritable awKey = new AirportWritable();
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String[] columns = value.toString().split(",");
         if (key.get() > 0) {
-
+    
         }
     }
 }
