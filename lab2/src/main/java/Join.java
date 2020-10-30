9c5710205923c6d9b31437792c38fb1e7e88093c
amaguma <amaguma5151@gmail.com>
2020-10-31 02:57:04 +0300

    Scripted auto-commit on change (2020-10-31 02:57:04) by gitwatch.sh

--- lab2/src/main/java/Join.java
+++ lab2/src/main/java/Join.java
@@ -1,7 +1,10 @@
+import org.apache.hadoop.mapreduce.Job;
+
 public class Join {
     public static void main(String[] args) throws Exception {
         if (args.length != 3) {
             System.exit(-1);
         }
+        Job job = 
     }
 }
