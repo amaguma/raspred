80d2ef087a6fc4ee5fb37a8833628959550cd180
amaguma <amaguma5151@gmail.com>
2020-10-31 01:51:16 +0300

    Scripted auto-commit on change (2020-10-31 01:51:16) by gitwatch.sh

--- lab2/src/main/java/AirportPartitioner.java
+++ lab2/src/main/java/AirportPartitioner.java
@@ -2,5 +2,6 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Partitioner;
 
 public class AirportPartitioner extends Partitioner<AirportWritable, Text> {
+    @Override
     
 }
