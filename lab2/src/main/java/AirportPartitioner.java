9d49601aa01b3c5584c797fbae8238a32db7bc4b
amaguma <amaguma5151@gmail.com>
2020-10-31 18:00:06 +0300

    lab2

--- lab2/src/main/java/AirportPartitioner.java
+++ lab2/src/main/java/AirportPartitioner.java
@@ -4,6 +4,6 @@ import org.apache.hadoop.mapreduce.Partitioner;
 public class AirportPartitioner extends Partitioner<AirportWritable, Text>{
     @Override
     public int getPartition(AirportWritable key, Text value, int numReduceTasks) {
-        return Integer.hashCode(key.getId() & Integer.MAX_VALUE) % numReduceTasks;
+        return key.getId() % numReduceTasks;
     }
 }
