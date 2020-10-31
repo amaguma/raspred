1e834c143f54a6b4d1b26907eaab9faaea839085
amaguma <amaguma5151@gmail.com>
2020-10-31 17:50:54 +0300

    lab2

--- lab2/src/main/java/AirportReducer.java
+++ lab2/src/main/java/AirportReducer.java
@@ -1,31 +1,30 @@
 
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
 import java.util.Iterator;
 
-public class AirportReducer extends Reducer<AirportWritable, Text, Text, Text> {
+public class AirportReducer extends Reducer<AirportWritable, Text, IntWritable, Text> {
     @Override
     protected void reduce(AirportWritable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
         Iterator<Text> iterator = values.iterator();
-        Text airportName = iterator.next();
+        String airportName = iterator.next().toString();
         double max = Double.MIN_VALUE;
         double min = Double.MAX_VALUE;
         double count = 0;
         double sum = 0;
-        while(iterator.hasNext()) {
+        while (iterator.hasNext()) {
             double delay = Double.parseDouble(iterator.next().toString());
             sum += delay;
             count++;
-            if (delay < min) {
-                min = delay;
-            }
-            if (delay > max) {
-                max = delay;
-            }
+            max = Math.max(delay, max);
+            min = Math.min(delay, min);
         }
-        sum /= count;
-        context.write(airportName, new Text(" " + String.format("%f %f %f", min, sum, max)));
+        if (count > 0) {
+            sum /= count;
+            context.write(new IntWritable(key.getId()), new Text(String.format("%s %.2f %.2f %.2f",airportName, min, sum, max)));
+        }
     }
 }
--- lab2/src/main/java/FlightMapper.java
+++ lab2/src/main/java/FlightMapper.java
@@ -5,17 +5,17 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 public class FlightMapper extends Mapper<LongWritable, Text, AirportWritable, Text> {
+    private static final int AIRPORT_ID = 14;
+    private static final int DELAY = 18;
 
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String[] columns = value.toString().split(",");
         if (key.get() > 0) {
-            int id = Integer.parseInt(columns[14]);
-            double delay = Double.parseDouble(columns[18]);
-            if (delay == 0) {
-                return;
+            if (columns[DELAY].trim().length() != 0) {
+                int id = Integer.parseInt(columns[AIRPORT_ID]);
+                context.write(new AirportWritable(id, 1), new Text(columns[DELAY]));
             }
-            context.write(new AirportWritable(id, 1), new Text(columns[18]));
         }
     }
 }
