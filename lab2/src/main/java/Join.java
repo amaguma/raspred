24b843ae936e424662a07e980b80ebbc026e6ed4
amaguma <amaguma5151@gmail.com>
2020-10-31 03:05:11 +0300

    Scripted auto-commit on change (2020-10-31 03:05:11) by gitwatch.sh

--- lab2/src/main/java/Join.java
+++ lab2/src/main/java/Join.java
@@ -1,4 +1,5 @@
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapred.FileOutputFormat;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
@@ -13,6 +14,7 @@ public class Join {
         job.setJobName("Airport join");
         MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, AirportMapper.class);
         MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, FlightMapper.class);
-        
+
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
     }
 }
