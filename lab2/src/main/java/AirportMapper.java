d99bb2d7e83b0c5a2bc4e15ba57c3b77c87f6035
amaguma <amaguma5151@gmail.com>
2020-11-07 00:17:38 +0300

    Scripted auto-commit on change (2020-11-07 00:17:38) by gitwatch.sh

--- lab2/src/main/java/AirportMapper.java
+++ lab2/src/main/java/AirportMapper.java
@@ -10,11 +10,13 @@ public class AirportMapper extends Mapper<LongWritable, Text, AirportWritable, T
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String[] columns = value.toString().split(",");
         if (key.get() > 0) {
-            int id = Integer.parseInt(columns[0].replaceAll("\"",""));
-            String name = columns[1].replaceAll("\"","");
+            int id = Integer.parseInt(replaceSlashes(columns[0], "\"", ""));
+            String name = replaceSlashes(columns[1], "\"","");
             context.write(new AirportWritable(id, AirportWritable.Indicator.AIRPORT), new Text(name));
         }
     }
 
-    private void replaceSlashes(String regex, String replacement)
+    private String replaceSlashes(String str, String regex, String replacement) {
+        return str.replaceAll(regex, replacement);
+    }
 }
