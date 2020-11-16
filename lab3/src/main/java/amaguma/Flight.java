e320adf2fceb0ce4ee787b8158032b53729efecf
amaguma <amaguma5151@gmail.com>
2020-11-16 19:04:46 +0300

    Scripted auto-commit on change (2020-11-16 19:04:46) by gitwatch.sh

--- lab3/src/main/java/amaguma/Flight.java
+++ lab3/src/main/java/amaguma/Flight.java
@@ -4,5 +4,5 @@ public class Flight {
     private String departureId;
     private String destinationId;
     private double delay;
-    private boolean isCancelled
+    private boolean isCancelled;
 }
