9dfbe9338fac931c74d9be4dd6efef891e108c46
amaguma <amaguma5151@gmail.com>
2020-11-18 18:14:08 +0300

    Scripted auto-commit on change (2020-11-18 18:14:07) by gitwatch.sh

--- lab3/src/main/java/amaguma/Airport.java
+++ lab3/src/main/java/amaguma/Airport.java
@@ -4,14 +4,20 @@ import scala.Serializable;
 
 public class Airport implements Serializable {
     public String name;
+    public int id;
 
     Airport() {}
 
-    Airport(String name) {
+    Airport(String name, int id) {
         this.name = name;
+        this.id = id;
     }
 
     public String getName() {
         return this.name;
+    }
+
+    public int getId() {
+        return this.id;
     }
 }
