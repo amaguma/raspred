180d0cb3f32b07d1e47b54fdb4cf8ad902c6ff1d
amaguma <amaguma5151@gmail.com>
2020-11-18 18:34:09 +0300

    Scripted auto-commit on change (2020-11-18 18:34:09) by gitwatch.sh

--- lab3/src/main/java/amaguma/Application.java
+++ lab3/src/main/java/amaguma/Application.java
@@ -4,6 +4,7 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.broadcast.Broadcast;
 import scala.Tuple2;
 
 public class Application {
@@ -36,5 +37,6 @@ public class Application {
                     String name = str.substring(ind + 1);
                     return new Tuple2<>(airportId, new Airport(name, Integer.parseInt(airportId)));
                 });
+        final Broadcast
     }
 }
