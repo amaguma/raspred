fc333d3099161e0ac417c30cf33084ea989aebc7
amaguma <amaguma5151@gmail.com>
2020-11-18 17:44:26 +0300

    Scripted auto-commit on change (2020-11-18 17:44:26) by gitwatch.sh

--- lab3/src/main/java/amaguma/Application.java
+++ lab3/src/main/java/amaguma/Application.java
@@ -3,6 +3,8 @@ package amaguma;
 public class Application {
 
     public static void main(String[] args) {
-        if (args.length() != 3)
+        if (args.length() != 3) {
+            
+        }
     }
 }
