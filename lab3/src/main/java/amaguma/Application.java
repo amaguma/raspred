2a2ba8150f726fb52bd05ddfd9c1898308bb3a51
amaguma <amaguma5151@gmail.com>
2020-11-19 01:51:21 +0300

    Scripted auto-commit on change (2020-11-19 01:51:21) by gitwatch.sh

--- lab3/src/main/java/amaguma/Application.java
+++ lab3/src/main/java/amaguma/Application.java
@@ -75,7 +75,6 @@ public class Application {
                         if (flight.isCancelled()) {
                             cancelledFlights++;
                         }
-                            
                     }
                 });
     }
