23e1ab1977958d0c89ef8f28f5c16e47c26ccbf0
amaguma <amaguma5151@gmail.com>
2020-11-18 17:40:39 +0300

    Scripted auto-commit on change (2020-11-18 17:40:39) by gitwatch.sh

--- lab3/src/main/java/amaguma/Airport.java
+++ lab3/src/main/java/amaguma/Airport.java
@@ -3,4 +3,13 @@ package amaguma;
 import scala.Serializable;
 
 public class Airport implements Serializable {
+    public String name;
+
+    Airport() {}
+
+    Airport(String name) {
+        this.name = name;
+    }
+
+    
 }
