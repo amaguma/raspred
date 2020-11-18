6c0bdf9191d3ad76eca471b57e0d9ffd3046a42a
amaguma <amaguma5151@gmail.com>
2020-11-18 17:45:12 +0300

    Scripted auto-commit on change (2020-11-18 17:45:11) by gitwatch.sh

--- lab3/src/main/java/amaguma/Application.java
+++ lab3/src/main/java/amaguma/Application.java
@@ -1,5 +1,7 @@
 package amaguma;
 
+import org.apache.spark.SparkConf;
+
 public class Application {
 
     public static void main(String[] args) {
@@ -7,6 +9,6 @@ public class Application {
             System.exit(-1);
         }
 
-        
+        SparkConf 
     }
 }
