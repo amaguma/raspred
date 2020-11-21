4b86e4982ec76ad14750a0b3c39c485755b68e12
amaguma <amaguma5151@gmail.com>
2020-11-21 20:28:25 +0300

    Scripted auto-commit on change (2020-11-21 20:28:25) by gitwatch.sh

--- lab3/src/main/java/amaguma/Application.java
+++ lab3/src/main/java/amaguma/Application.java
@@ -13,6 +13,7 @@ import java.util.Map;
 public class Application {
 
     private static final int DEPARTURE_AIRPORT_ID = 11;
+    private static final int DESTINATION_AIRPORT_ID = 11;
 
     public static JavaRDD<String> removeHeader(JavaRDD<String> file) {
         return file.filter(str -> str.equals(file.first()));
