35e80ca52f170b54492e3835259fe74a53b18210
amaguma <amaguma5151@gmail.com>
2020-12-09 01:32:30 +0300

    Scripted auto-commit on change (2020-12-09 01:32:30) by gitwatch.sh

--- lab4/src/main/java/lab4/AkkaApplication.java
+++ lab4/src/main/java/lab4/AkkaApplication.java
@@ -1,11 +1,16 @@
 package lab4;
 
+import akka.NotUsed;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.server.AllDirectives;
 import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class AkkaApplication extends AllDirectives {
     private ActorRef actorRouter;
@@ -19,6 +24,10 @@ public class AkkaApplication extends AllDirectives {
         ActorRef actorRouter = system.actorOf(Props.create(RouterActor.class, system), "actorRouter");
 
         final Http http = Http.get(system);
-        final ActorMaterializer ma
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+
+        AkkaApplication instance = new AkkaApplication(actorRouter);
+
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = instance
     }
 }
