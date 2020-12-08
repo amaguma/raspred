f6e19cc1d99c0ca7e80537a7765a8b385018df8d
amaguma <amaguma5151@gmail.com>
2020-12-08 18:23:01 +0300

    Scripted auto-commit on change (2020-12-08 18:23:01) by gitwatch.sh

--- lab4/src/main/java/lab4/StorageActor.java
+++ lab4/src/main/java/lab4/StorageActor.java
@@ -1,4 +1,10 @@
 package lab4;
 
-public class StorageActor {
+import akka.actor.AbstractActor;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class StorageActor extends AbstractActor {
+    private Map<String, ArrayList<>>
 }
