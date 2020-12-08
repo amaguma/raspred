bfb99c0e2a60f546a0060a055387ea930aa482cc
amaguma <amaguma5151@gmail.com>
2020-12-08 23:55:19 +0300

    Scripted auto-commit on change (2020-12-08 23:55:19) by gitwatch.sh

--- lab4/src/main/java/lab4/TestResult.java
+++ lab4/src/main/java/lab4/TestResult.java
@@ -1,4 +1,25 @@
 package lab4;
 
-public class TestResults {
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.io.Serializable;
+
+public class TestResult implements Serializable {
+    private final String EXPECTED = "expected";
+    private final String ACTUAL = "actual";
+    private final String SUCCES = "succes";
+
+    private String expectedResult;
+    private String actualResult;
+    private Boolean succes;
+
+    @JsonCreator
+    public TestResult(@JsonProperty(EXPECTED) String expectedResult,
+                      @JsonProperty(ACTUAL) String actualResult,
+                      @JsonProperty(SUCCES) Boolean succes) {
+        this.expectedResult = expectedResult;
+        this.actualResult = actualResult;
+        this.succes = succes;
+    }
 }
