376f656f0cbc029b5713862cdc0bd01354c5cd63
amaguma <amaguma5151@gmail.com>
2020-12-08 19:03:35 +0300

    Scripted auto-commit on change (2020-12-08 19:03:35) by gitwatch.sh

--- lab4/src/main/java/lab4/TestPackage.java
+++ lab4/src/main/java/lab4/TestPackage.java
@@ -1,14 +1,19 @@
 package lab4;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.io.Serializable;
 import java.util.List;
 
 public class TestPackage implements Serializable {
+
+    private final String PACKAGE_ID = "packageId";
+    private final String 
 
     private String packageId;
     private String functionName;
     private String jsScript;
     private List<TestData> tests;
 
-    public TestPackage
+    public TestPackage (@JsonProperty())
 }
