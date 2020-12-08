eff31157665452d3d0729001ca3265754ea01e3e
amaguma <amaguma5151@gmail.com>
2020-12-08 18:44:31 +0300

    Scripted auto-commit on change (2020-12-08 18:44:30) by gitwatch.sh

--- lab4/src/main/java/lab4/TestData.java
+++ lab4/src/main/java/lab4/TestData.java
@@ -7,15 +7,21 @@ import java.io.Serializable;
 
 public class TestData implements Serializable {
 
-
+    
 
     private String testName;
     private String actualResult;
     private String expectedResult;
-    private Object params;
+    private Object[] params;
 
     @JsonCreator
-    public TestData(@JsonProperty("testName") String)
+    public TestData(@JsonProperty("testName") String testName,
+                    @JsonProperty("expectedResult") String expectedResult,
+                    @JsonProperty("params") Object[] params) {
+        this.testName = testName;
+        this.expectedResult = expectedResult;
+        this.params = params;
+    }
 
 }
 
