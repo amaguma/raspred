e8f205f5bd21ef05416eda474502166444de56d3
amaguma <amaguma5151@gmail.com>
2020-12-09 01:04:35 +0300

    Scripted auto-commit on change (2020-12-09 01:04:35) by gitwatch.sh

--- lab4/src/main/java/lab4/TesterActor.java
+++ lab4/src/main/java/lab4/TesterActor.java
@@ -1,4 +1,20 @@
 package lab4;
 
-public class TesterActor {
+import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+public class TesterActor extends AbstractActor {
+    private ActorRef storageActor;
+
+    public TesterActor(ActorRef storageActor) {
+        this.storageActor = storageActor;
+    }
+
+    private String runTest(TestData testData) throws ScriptException, NoSuchMethodException {
+        ScriptEngine engine = new ScriptEngineManager().getEngineByName()
+    }
 }
