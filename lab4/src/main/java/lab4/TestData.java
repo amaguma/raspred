fbf4cbac8c5c3e7df98bc4885697dbc0f9c819fb
amaguma <amaguma5151@gmail.com>
2020-12-08 18:41:05 +0300

    Scripted auto-commit on change (2020-12-08 18:41:05) by gitwatch.sh

--- lab4/src/main/java/lab4/TestData.java
+++ lab4/src/main/java/lab4/TestData.java
@@ -3,12 +3,16 @@ package lab4;
 import java.io.Serializable;
 
 public class TestData implements Serializable {
+
+
+
     private String testName;
     private String actualResult;
     private String expectedResult;
     private Object params;
 
     
+
 }
 
 
