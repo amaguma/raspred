ddce9ccef10a66411b59f2a0275d2dc9ce17acaa
amaguma <amaguma5151@gmail.com>
2020-12-12 00:11:15 +0300

    Scripted auto-commit on change (2020-12-12 00:11:15) by gitwatch.sh

--- lab4/src/main/java/lab4/Result.java
+++ lab4/src/main/java/lab4/Result.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Result implements Serializable {
@@ -12,11 +13,11 @@ public class Result implements Serializable {
     private final String TESTS = "tests";
 
     private String packageId;
-    private List<TestResult> tests;
+    private ArrayList<TestResult> tests;
 
     @JsonCreator
     public Result(@JsonProperty(PACKAGE_ID) String packageId,
-                  @JsonProperty(TESTS) List<TestResult> tests) {
+                  @JsonProperty(TESTS) ArrayList<TestResult> tests) {
         this.packageId = packageId;
         this.tests = tests;
     }
