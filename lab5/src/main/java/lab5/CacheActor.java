df324edd6088c6b7ead4802cc01e36096f4cdee2
amaguma <amaguma5151@gmail.com>
2020-12-12 20:11:08 +0300

    Scripted auto-commit on change (2020-12-12 20:11:08) by gitwatch.sh

--- lab5/src/main/java/lab5/CacheActor.java
+++ lab5/src/main/java/lab5/CacheActor.java
@@ -2,6 +2,11 @@ package lab5;
 
 import akka.actor.AbstractActor;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CacheActor extends AbstractActor {
-    private final Map<>
+    private final Map<String, Integer> cache = new HashMap<>();
+
+    
 }
