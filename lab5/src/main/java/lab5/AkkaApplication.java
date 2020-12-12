8e1c32854da40f3e22a091d61a823480d8da2231
amaguma <amaguma5151@gmail.com>
2020-12-12 20:31:26 +0300

    Scripted auto-commit on change (2020-12-12 20:31:26) by gitwatch.sh

--- lab5/src/main/java/lab5/AkkaApplication.java
+++ lab5/src/main/java/lab5/AkkaApplication.java
@@ -1,6 +1,12 @@
 package lab5;
 
+import akka.NotUsed;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.stream.javadsl.Flow;
+
+
 public class AkkaApplication {
 
-    private 
+    public static Flow<HttpRequest, HttpResponse, NotUsed> create
 }
