06100773668198cd9a99d96c6aaab42ffb07bcc3
amaguma <amaguma5151@gmail.com>
2020-12-23 00:36:30 +0300

    Scripted auto-commit on change (2020-12-23 00:36:29) by gitwatch.sh

--- lab6/src/main/java/lab6/StorageActor.java
+++ lab6/src/main/java/lab6/StorageActor.java
@@ -2,10 +2,16 @@ package lab6;
 
 import akka.actor.AbstractActor;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class StorageActor extends AbstractActor {
     private List<String> storage;
     private final Random random;
+
+    public StorageActor() {
+        this.storage = new ArrayList<>();
+        this.random = new Random();
+    }
 }
