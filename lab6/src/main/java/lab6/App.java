59173c0219e059fab43a9bd3b6265a3e03d9c98d
amaguma <amaguma5151@gmail.com>
2020-12-23 18:36:08 +0300

    Scripted auto-commit on change (2020-12-23 18:36:08) by gitwatch.sh

--- lab6/src/main/java/lab6/App.java
+++ lab6/src/main/java/lab6/App.java
@@ -1,7 +1,10 @@
 package lab6;
 
+import akka.actor.ActorRef;
+import akka.http.javadsl.Http;
 import akka.http.javadsl.server.AllDirectives;
 
 public class App extends AllDirectives {
-    
+    private Http http;
+    private ActorRef storeActor;
 }
