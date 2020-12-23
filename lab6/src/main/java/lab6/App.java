9b99b6339092e3609944067f4a5d68f0ead19b56
amaguma <amaguma5151@gmail.com>
2020-12-23 18:44:55 +0300

    Scripted auto-commit on change (2020-12-23 18:44:55) by gitwatch.sh

--- lab6/src/main/java/lab6/App.java
+++ lab6/src/main/java/lab6/App.java
@@ -2,19 +2,24 @@ package lab6;
 
 import akka.actor.ActorRef;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
 import akka.http.javadsl.server.AllDirectives;
+import akka.http.javadsl.server.Route;
 
-import java.net.http.HttpResponse;
 import java.util.concurrent.CompletionStage;
 
 public class App extends AllDirectives {
-    private Http http;
+    private static Http http;
     private ActorRef storeActor;
     private int port;
 
-    
 
     private static CompletionStage<HttpResponse> fetch(String url) {
-        return
+        return http.singleRequest(HttpRequest.create(url));
+    }
+
+    public Route createRoute() {
+        return 
     }
 }
