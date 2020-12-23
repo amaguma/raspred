0d712f151e13e13cd2cf7186c927dfb71a062031
amaguma <amaguma5151@gmail.com>
2020-12-23 20:53:22 +0300

    Scripted auto-commit on change (2020-12-23 20:53:22) by gitwatch.sh

--- lab6/src/main/java/lab6/App.java
+++ lab6/src/main/java/lab6/App.java
@@ -2,6 +2,7 @@ package lab6;
 
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import akka.actor.Props;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
@@ -52,6 +53,7 @@ public class App extends AllDirectives {
 
     public static void main(String[] args) throws IOException, KeeperException, InterruptedException {
         final ActorSystem system = ActorSystem.create("routes");
+        ActorRef storageActor = system.actorOf(Props.create(StorageActor.class));
         
     }
 }
