499e87bff7aa4906bb375bd246857d972ad66536
amaguma <amaguma5151@gmail.com>
2020-12-23 17:30:42 +0300

    Scripted auto-commit on change (2020-12-23 17:30:42) by gitwatch.sh

--- lab6/src/main/java/lab6/Server.java
+++ lab6/src/main/java/lab6/Server.java
@@ -1,15 +1,18 @@
 package lab6;
 
 import akka.actor.ActorRef;
+import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
+
+import java.util.List;
 
 public class Server implements Watcher {
     private ZooKeeper zooKeeper;
     private ActorRef storeActor;
     private int port;
 
-    public Server(ActorRef storeActor, int port) {
-
+    public Server(ActorRef storeActor, int port) throws KeeperException, InterruptedException {
+        List<String> servers = 
     }
 }
