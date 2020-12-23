7311de12f481cfc2efdbf64d02fef97a88e47f56
amaguma <amaguma5151@gmail.com>
2020-12-23 18:15:31 +0300

    Scripted auto-commit on change (2020-12-23 18:15:31) by gitwatch.sh

--- lab6/src/main/java/lab6/Zoo.java
+++ lab6/src/main/java/lab6/Zoo.java
@@ -3,52 +3,58 @@ package lab6;
 import akka.actor.ActorRef;
 import org.apache.zookeeper.*;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Zoo implements Watcher {
+public class Zoo {
     private ZooKeeper zooKeeper;
     private ActorRef storeActor;
     private int port;
 
-    public Zoo(ActorRef storeActor, int port) throws KeeperException, InterruptedException {
+    public Zoo(ActorRef storeActor, int port) {
+        try {
+            this.zooKeeper = new ZooKeeper(
+                    "127.0.0.1:2181",
+                    5000,
+                    null
+            );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         this.storeActor = storeActor;
         this.port = port;
-        this.zooKeeper = new ZooKeeper(
-                "127.0.0.1:2181",
-                5000,
-                null
-        );
         sendServers();
+        initZoo();
     }
 
-    public void sendServers() throws KeeperException, InterruptedException {
-        List<String> serversName = zooKeeper.getChildren("/servers", watchedEvent -> {
-            if (watchedEvent.getType() == Event.EventType.NodeChildrenChanged) {
-                sendServers();
-            }
-        });
-        List<String> servers = new ArrayList<>();
-        for (String s : serversName) {
-            byte[] port = zooKeeper.getData("/servers/" + s, false, null);
-        }
-        this.storeActor.tell(new ServerMsg(servers), ActorRef.noSender());
-    }
-
-    public void initZoo() {
-        this.zooKeeper.create("/servers/localhost:" + this.port,
-                String.valueOf(this.port).getBytes(),
-                ZooDefs.Ids.OPEN_ACL_UNSAFE,
-                CreateMode.EPHEMERAL
-                );
-    }
-
-    @Override
-    public void process(WatchedEvent watchedEvent) {
+    public void sendServers() {
         try {
-            sendServers();
+            List<String> serversName = zooKeeper.getChildren("/servers", watchedEvent -> {
+                if (watchedEvent.getType() == Watcher.Event.EventType.NodeChildrenChanged) {
+                    sendServers();
+                }
+            });
+
+            List<String> servers = new ArrayList<>();
+            for (String s : serversName) {
+                byte[] port = zooKeeper.getData("/servers/" + s, false, null);
+            }
+            this.storeActor.tell(new ServerMsg(servers), ActorRef.noSender());
         } catch (KeeperException | InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    public void initZoo() {
+       try {
+           this.zooKeeper.create("/servers/localhost:" + this.port,
+                   String.valueOf(this.port).getBytes(),
+                   ZooDefs.Ids.OPEN_ACL_UNSAFE,
+                   CreateMode.EPHEMERAL
+           );
+       } catch (KeeperException | InterruptedException e) {
+           e.printStackTrace();
+       }
     }
 }
