17f609cd55f13879397ae72c80abfd5019a107de
amaguma <amaguma5151@gmail.com>
2020-12-24 23:37:29 +0300

    Scripted auto-commit on change (2020-12-24 23:37:28) by gitwatch.sh

--- lab7/src/main/java/lab7/Client.java
+++ lab7/src/main/java/lab7/Client.java
@@ -13,6 +13,9 @@ public class Client {
     private static final String SET_REQ = "SET";
     private static final int GET_REQ_LENGTH = 2;
     private static final int SET_REQ_LENGTH = 3;
+    private static final int INDEX_REQ = 0;
+    private static final int INDEX_KEY = 1;
+    private static final int INDEX_VALUE = 2;
 
     public static void main(String[] args) {
         ZContext context = new ZContext();
@@ -29,15 +32,15 @@ public class Client {
 
             String[] commands = command.split(DELIMITER);
 
-            if (commands.length == GET_REQ_LENGTH && commands[0].equals(GET_REQ)) {
-                int key = Integer.parseInt(commands[1]);
+            if (commands.length == GET_REQ_LENGTH && commands[INDEX_REQ].equals(GET_REQ)) {
+                int key = Integer.parseInt(commands[INDEX_KEY]);
 
                 String str = String.format(GET_REQ + " %d", key);
                 String response = sendAndReceive(socket, str);
                 System.out.println(response);
-            } else if (commands.length == SET_REQ_LENGTH && commands[0].equals(SET_REQ)) {
-                int key = Integer.parseInt(commands[1]);
-                int value = Integer.parseInt(commands[2]);
+            } else if (commands.length == SET_REQ_LENGTH && commands[INDEX_REQ].equals(SET_REQ)) {
+                int key = Integer.parseInt(commands[INDEX_KEY]);
+                int value = Integer.parseInt(commands[INDEX_VALUE]);
 
                 String str = String.format(SET_REQ + " %d %d", key, value);
                 String response = sendAndReceive(socket, str);
