342c45fdfca950fc7ec636e08a1831694020e44d
amaguma <amaguma5151@gmail.com>
2020-12-24 20:40:54 +0300

    Scripted auto-commit on change (2020-12-24 20:40:54) by gitwatch.sh

--- lab7/src/main/java/lab7/Config.java
+++ lab7/src/main/java/lab7/Config.java
@@ -5,5 +5,55 @@ import org.zeromq.ZFrame;
 public class Config {
     private ZFrame address;
     private String id;
-    private long 
+    private long time;
+    private int min;
+    private int max;
+
+    public Config(ZFrame address, String id, long time, int min, int max) {
+        this.address = address;
+        this.id = id;
+        this.time = time;
+        this.min = min;
+        this.max = max;
+    }
+
+    public void setAddress(ZFrame address) {
+        this.address = address;
+    }
+
+    public ZFrame getAddress() {
+        return this.address;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    public long getTime() {
+        return this.time;
+    }
+
+    public void setMin(int min) {
+        this.min = min;
+    }
+
+    public int getMin() {
+        return this.min;
+    }
+
+    public void setMax(int max) {
+        this.max = max;
+    }
+
+    public int getMax() {
+        return this.max;
+    }
 }
