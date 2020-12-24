bdf68e889ab3c4628817c2b870aae65f050603f0
amaguma <amaguma5151@gmail.com>
2020-12-24 20:54:34 +0300

    Scripted auto-commit on change (2020-12-24 20:54:34) by gitwatch.sh

--- lab7/src/main/java/lab7/Proxy.java
+++ lab7/src/main/java/lab7/Proxy.java
@@ -1,10 +1,21 @@
 package lab7;
 
+import org.zeromq.SocketType;
+import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
 
 import java.util.ArrayList;
 
 public class Proxy {
     private static ArrayList<Config> configs;
-    private static ZMQ.Socket front
+    private static ZMQ.Socket frontend;
+    private static ZMQ.Socket backend;
+
+    public static void main(String[] args) {
+        ZContext context = new ZContext();
+        frontend = context.createSocket(SocketType.ROUTER);
+        frontend.bind("tcp://localhost:5556");
+        backend = context.createSocket(SocketType.ROUTER);
+        backend.bind("tcp://localhost:5555");
+    }
 }
