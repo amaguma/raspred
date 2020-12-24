e4df375b776c82cc319619d8b5ea8a82aea53809
amaguma <amaguma5151@gmail.com>
2020-12-24 23:41:56 +0300

    Scripted auto-commit on change (2020-12-24 23:41:55) by gitwatch.sh

--- lab7/src/main/java/lab7/Tools.java
+++ lab7/src/main/java/lab7/Tools.java
@@ -16,5 +16,17 @@ public class Tools {
     private static final int MESSAGE_SIZE = 3;
     private static final String DELIMITER = " ";
 
+    private static final String SOCKET_ADDRESS = "tcp://localhost:5556";
+    private static final String DELIMITER = " ";
+    private static final String EMPTY_COMMAND = "";
+    private static final String GET_REQ = "GET";
+    private static final String SET_REQ = "SET";
+    private static final int GET_REQ_LENGTH = 2;
+    private static final int SET_REQ_LENGTH = 3;
+    private static final int INDEX_REQ = 0;
+    private static final int INDEX_KEY = 1;
+    private static final int INDEX_VALUE = 2;
+    private static final String WRONG_COMMAND = "Wrong command";
+    private static final String EMPTY_MSG = "Empty message";
 
 }
