efbbaf3eb1d42129ed1ec9b4f643e0bb6cfbe087
amaguma <amaguma5151@gmail.com>
2020-12-24 19:17:10 +0300

    Scripted auto-commit on change (2020-12-24 19:17:10) by gitwatch.sh

--- lab7/src/main/java/lab7/Storage.java
+++ lab7/src/main/java/lab7/Storage.java
@@ -2,6 +2,7 @@ package lab7;
 
 public class Storage {
     public static void main(String[] args) {
-        
+        int min = Integer.parseInt(args[0]);
+        int max = Integer.parseInt(args[1]);
     }
 }
