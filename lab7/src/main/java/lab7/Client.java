a080db6763a52424c2ab4af2ceb16c887a520ccf
amaguma <amaguma5151@gmail.com>
2020-12-24 22:39:41 +0300

    Scripted auto-commit on change (2020-12-24 22:39:41) by gitwatch.sh

--- lab7/src/main/java/lab7/Client.java
+++ lab7/src/main/java/lab7/Client.java
@@ -4,10 +4,14 @@ import org.zeromq.SocketType;
 import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
 
+import java.util.Scanner;
+
 public class Client {
     public static void main(String[] args) {
         ZContext context = new ZContext();
         ZMQ.Socket socket = context.createSocket(SocketType.REQ);
         socket.connect("tcp://localhost:5556");
+
+        Scanner in = new Scanner(System.in);
     }
 }
