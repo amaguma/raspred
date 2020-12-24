236f8997821f11a87a0bcac19a83a4ddf650c3c4
amaguma <amaguma5151@gmail.com>
2020-12-24 19:19:01 +0300

    Scripted auto-commit on change (2020-12-24 19:19:01) by gitwatch.sh

--- lab7/src/main/java/lab7/Storage.java
+++ lab7/src/main/java/lab7/Storage.java
@@ -1,8 +1,13 @@
 package lab7;
 
+import org.zeromq.ZContext;
+
 public class Storage {
     public static void main(String[] args) {
         int min = Integer.parseInt(args[0]);
         int max = Integer.parseInt(args[1]);
+
+        ZContext context = new ZContext();
+        
     }
 }
