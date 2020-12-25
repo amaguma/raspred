b23cf55e85c66812f65681481846abde13af6591
amaguma <amaguma5151@gmail.com>
2020-12-25 19:39:03 +0300

    Scripted auto-commit on change (2020-12-25 19:39:03) by gitwatch.sh

--- lab7/src/main/java/lab7/Storage.java
+++ lab7/src/main/java/lab7/Storage.java
@@ -45,9 +45,7 @@ public class Storage {
 
                     String[] commands = Tools.splitStr(command);
 
-                    for (String s : commands) {
-                        System.out.println(s);
-                    }
+
 
                     if (commands.length == Tools.GET_REQ_LENGTH && commands[Tools.INDEX_REQ].equals(Tools.GET_REQ)) {
                         int key = Integer.parseInt(commands[Tools.INDEX_KEY]);
